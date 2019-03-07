397d3721be3c2a5df29089b8cf6af8944834533b
hyeonukseo <48095615+hyeonukseo@users.noreply.github.com>
2019-03-07 22:05:20 +0900

    OX 문제
    점수계산 문제

--- Algorithm/OX문제/소스.c
+++ Algorithm/OX문제/소스.c
@@ -3,6 +3,32 @@
 int main() {
 
 	//Please Enter Your Code Here
+	int n; //input
+	int i; //loop
+	int a[100]; // max_storage
+	int sum=0; // score_sum
+	int count = 1;
+
+	scanf("%d", &n);
+	for (i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+
+	for (i = 0; i < n; i++)
+	{
+		if (a[i] != 0)
+		{
+			sum = sum + count;
+			count++;
+		}
+		else
+		{
+			count = 1;
+		}
+	}
+
+	printf("%d", sum);
 
 	return 0;
 }
