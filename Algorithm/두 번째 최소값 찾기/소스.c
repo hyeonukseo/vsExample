c46308be1ab17d234ad4399737b8563637f3b9da
hyeonukseo <48095615+hyeonukseo@users.noreply.github.com>
2019-03-07 17:54:24 +0900

    Update 소스.c
    great

--- Algorithm/두 번째 최소값 찾기/소스.c
+++ Algorithm/두 번째 최소값 찾기/소스.c
@@ -4,14 +4,36 @@ int main() {
 
 	//Please Enter Your Code Here
 
-	int a[9], i,j;
+	int i;
+	int a[9];
+	int min, sMin;
+	int l;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 9; i++) //ÀÔ·Â
 	{
 		scanf("%d", &a[i]);
 	}
 
+	min = sMin = a[0];
 
+	for (i = 1; i < 9; i++)
+	{
+		if (a[i] < min)
+		{
+			sMin = min;
+			min = a[i];
+			l = i;
+
+		}
+		else if (a[i] > min && a[i] < sMin || min == sMin)
+		{
+			sMin = a[i];
+			l = i + 1;
+		}
+
+	}
+
+	printf("%d\n%d\n", sMin, l);
 
 	return 0;
 }
