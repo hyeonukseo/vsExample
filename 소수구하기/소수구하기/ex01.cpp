c72c73c5158173a70a729ffec548d607c15cd185
hyeonukseo <48095615+hyeonukseo@users.noreply.github.com>
2019-03-07 15:40:19 +0900

    22

---
+++ 소수구하기/소수구하기/ex01.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+
+	//Please Enter Your Code Here
+	int a, b, c, i;
+
+	scanf("%d %d", &a, &b);
+
+	for (i = a; i <= b; i++)
+	{
+		for (c = 2; i%c != 0; c++)
+		if (i == 1)
+			break;
+
+		if (c == i)
+			printf("%d ", c);
+		else
+			c = 2;
+	}//
+	return 0;
+}
