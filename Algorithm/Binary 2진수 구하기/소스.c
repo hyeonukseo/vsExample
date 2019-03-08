bd08ce09d4dd600f5fb1e36d098af08111597c86
hyeonukseo <48095615+hyeonukseo@users.noreply.github.com>
2019-03-08 11:44:17 +0900

    binary
    2진수 구하기

---
+++ Algorithm/Binary 2진수 구하기/소스.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main() {
+
+	//Please Enter Your Code Here
+	int n;
+	int i,j;
+	int step;
+	int binary[10]; // n이 1000이하 이므로 2^10 -> 10개만
+
+	scanf("%d", &n);
+
+	for (i=0 ; ; i++ )
+	{
+		if (n / 2 >= 1) 
+		{
+			binary[i] = n % 2;
+			n = n / 2;
+		}
+		else
+		{
+			binary[i] = n % 2;
+			break;
+		}
+	}
+
+	for (j = i; j >= 0; j--)
+	{
+		printf("%d", binary[j]);
+	}
+
+	return 0;
+}
--- Algorithm/주사위 게임/소스.c
+++ Algorithm/주사위 게임/소스.c
@@ -21,19 +21,24 @@ int main() {
 
 	for (i = 0; i<n; i++)
 	{
-		if (a[i][0] == a[i][1] && a[i][1] == a[i][2])
+		
+		if (a[i][0] == a[i][1] && a[i][1] == a[i][2])           // 3개가 같을 때
 		{
 			total[i] = total[i] + 10000 + (a[i][0] * 1000);
 		}
-		else if (a[i][0] == a[i][1] || a[i][0] == a[i][2])
+		else if (a[i][0] == a[i][1] || a[i][0] == a[i][2])      // 2개가 같을 때
 		{
 			total[i] = total[i] + 1000 + (a[i][0] * 1000);
 		}
-		else if (a[i][1] == a[i][2])
+		else if (a[i][1] == a[i][2] || a[i][1] == a[i][0])
 		{
 			total[i] = total[i] + 1000 + (a[i][1] * 1000);
 		}
-		else
+		else if (a[i][2] == a[i][0] || a[i][2] == a[i][1])
+		{
+			total[i] = total[i] + 1000 + (a[i][2] * 1000);
+		}
+		else                                                    // 모두 다를 때.
 		{
 			max = a[i][0];
 			for (j = 1; j<3; j++)
@@ -45,13 +50,14 @@ int main() {
 			}
 			total[i] = total[i] + (100 * max);
 		}
+		
 	}
 
 	max = total[0];
 
 	for (i = 1; i<n; i++)
 	{
-		if (total[i]>max)
+		if (total[i]>=max)
 			max = total[i];
 	}
 
